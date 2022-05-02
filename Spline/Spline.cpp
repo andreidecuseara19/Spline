c5d1f6397ea2a4e679806461a987cea5b0cca573
andreidecuseara19 <andrei_decuseara@yahoo.com>
2022-05-02 10:53:57 +0300

    interpolated

--- Spline/Spline.cpp
+++ Spline/Spline.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <math.h>
 #include "CSpline.h"
+
+
+double read(double val, vector<double> xi, vector<double> yi, int nTotal)
+{
+	double mindiff = 1;
+	int pos;
+	for (int i = 0; i < nTotal; i++) {
+		if (abs(val - xi[i]) < mindiff)
+		{
+			mindiff = abs(val - xi[i]);
+			pos = i;
+		}
+	}
+	return yi[pos];
+}
+
 
 int main() {
 	double x[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -20,6 +37,11 @@ int main() {
 	for (int i = 0; i < nTotal; i++) {
 		cout << xi[i] << "\t" << yi[i] << endl;
 	}
+
+	double interpolated1 = read(1.75, xi, yi, nTotal);
+	cout << interpolated1 << endl;
+
+	//derivata se face scazand cele mai apropiate doua puncte x si facand Dy/Dx
 
 	delete pS;
 
